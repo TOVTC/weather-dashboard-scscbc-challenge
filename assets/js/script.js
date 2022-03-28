282f5df766ca4a2726ef76029dbacc6e5cc75455
TOVTC <veronica.tc.to@outlook.com>
2022-03-28 15:46:40 -0400

    pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+//start basic structure using Boostrap grid
+//send API requests to get location, (date?), current weather, weather for the next five days
+
+//set up API request function
+    //send an initial request and parse with JSON to determine what values are returned by general request (and how to access)
+    //set up function to retrieve the hardcoded locations on the left (to practice how to retrieve, access, generate values and elements)
+    //link to buttons on the left
+    //set up function to dynamically retrieve weather data for user input using existing API request function (capture user input, trim text, add as API parameter)
+//set up function to generate elements for current weather (for loop)
+//set up function to loop through weather for next five days (add as a second parameter?)
+//set up buttons using "data" property for hardcoded button list
+//set up ability for user input to be captured in form element and dynamically displayed
+//set up function to choose from a list of icons depending on the type of weather returned
+//add finishing touches with Bootstrap/custom CSS
