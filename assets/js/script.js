c7b2b61bbc9cf988dd0d91852b4ccbe1273570a9
TOVTC <veronica.tc.to@outlook.com>
2022-03-28 23:56:55 -0400

    added functions to select required data for current and forecast weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@
     //set up function to dynamically retrieve weather data for user input using existing API request function (capture user input, trim text, add as API parameter)
 //set up function to generate elements for current weather (for loop)
 //set up function to loop through weather for next five days (add as a second parameter?)
-//set up buttons using "data" property for hardcoded button list
+//set up buttons for search history using data-* property
 //set up ability for user input to be captured in form element and dynamically displayed
 //set up geolocation API from the openweather map software
 //set up function to choose from a list of icons depending on the type of weather returned
@@ -18,24 +18,50 @@
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=6b2366660c4d73ceba774993533fef58
 //http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=6b2366660c4d73ceba774993533fef58
 
-
-var testAPI = function() {
-    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=new+york&appid=6b2366660c4d73ceba774993533fef58";
+//geolocation API request
+var apiLatLon = function() {
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=toronto&appid=6b2366660c4d73ceba774993533fef58"; //don't forget to make city dynamic for user input
     fetch(apiUrl)
         .then(function(response) {
             response.json().then(function(data) {
                 console.log(data[0].lat, data[0].lon)
                 getWeather(data);
+                //also write a line of code to change the text of the heading to the name of the city
             })
         })
 }
 
 //test API request
 var getWeather = function(data) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&exclude=minutely,hourly&appid=6b2366660c4d73ceba774993533fef58";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&exclude=minutely,hourly&appid=6b2366660c4d73ceba774993533fef58&units=metric";
     fetch(apiUrl).then(function(response) {
-        console.log(response.json())
+        // console.log(response.json())
+        response.json().then(function(response) {
+            //call two separate functions - one for current and one for future
+            currentWeather(response);
+            forecastWeather(response);
+        })
     })
 }
 
-testAPI();
+var currentWeather = function(response) {
+    console.log(response.current.dt)
+    console.log(response.current.weather[0].icon);
+    console.log(response.current.temp);
+    console.log(response.current.wind_speed);
+    console.log(response.current.humidity);
+    console.log(response.current.uvi);
+}
+
+var forecastWeather = function(response) {
+    for (i = 0; i < 5; i++) {
+        console.log("***");
+        console.log(response.daily[i].dt);
+        console.log(response.daily[i].weather[0].icon);
+        console.log(response.daily[i].temp.day);
+        console.log(response.daily[i].wind_speed);
+        console.log(response.daily[i].humidity);
+    }
+}
+
+apiLatLon();
